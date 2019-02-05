84ee53e0c603e0433e4eaed551d7f9521002b953
topdeveloper0729 <topdev0729@gmail.com>
2019-02-05 14:46:15 -0800

    package.json created, knexfile.js created, db folder created, and index.js created.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Use-My-Tools-CSR
 My version of Build Week 1 Project
+
+This is the backend of my personal version of the Use My Tools project.
+
+I'm setting up a postgres database and creating/running migrations using knex. 
+
+I'll use an express server for dev before deploying to Heroku.
---
+++ knexfile.js
@@ -0,0 +1,39 @@
+// Database config settings
+
+module.exports = {
+  development: {
+    client: 'pg',
+    connection:'postgres://localhost/usemytools_dev_db',
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/dev'
+    },
+    useNullAsDefault: true
+  },
+
+  // test: {
+  //   client: 'pg',
+  //   connection:'postgres://localhost/<examples_test>',
+  //   migrations: {
+  //     directory: './db/migrations'
+  //   },
+  //   seeds: {
+  //     directory: './db/seeds/test'
+  //   },
+  //   useNullAsDefault: true
+  // },
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/production'
+    },
+    useNullAsDefault: true
+  }
+};
