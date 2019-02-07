ed28a4554183fcaa7d6443a13fae6d2a95ee961d
topdeveloper0729 <topdev0729@gmail.com>
2019-02-06 18:30:53 -0800

    About to run migration to create tool_images table

---
+++ db/migrations/20190206182421_create.js
@@ -0,0 +1,22 @@
+
+exports.up = function(knex, Promise) {
+    return Promise.all([
+        knex.schema.createTable('tool_images', table => {
+            table.integer('tool_id')
+                .notNullable()
+                .references('id')
+                .inTable('tools');
+            table.integer('image_id')
+                .notNullable()
+                .references('id')
+                .inTable('images');
+        
+            table.primary(['tool_id', 'img_id']);   // primary key is a combination of tool_id and image_id
+        })
+    ])
+    
+};
+
+exports.down = function(knex, Promise) {
+    return knex.schema.dropTableIfExists('tool_images');
+};
