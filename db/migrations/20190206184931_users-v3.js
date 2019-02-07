cfc5e42ba9cef52a82e179871205bf4f2ed33949
topdeveloper0729 <topdev0729@gmail.com>
2019-02-06 19:08:35 -0800

    About to run migration to drop and recreate users table with image_id

---
+++ db/migrations/20190206184931_users-v3.js
@@ -0,0 +1,27 @@
+exports.up = function(knex, Promise) {
+    return Promise.all([
+        knex.schema.createTable('users', table => {
+            table.increments('uid').primary();
+            table.string('username')
+                .unique()
+                .notNullable();
+            table.string('password').notNullable();
+            table.string('email')
+                .unique()
+                .notNullable();
+            table.string('firstname').notNullable();
+            table.string('lastname').notNullable();
+            table.string('street_address');
+            table.integer('image_id')
+                .notNullable()
+                .references('id')
+                .inTable('images');
+        })
+    ])
+};
+
+exports.down = function(knex, Promise) {
+    return Promise.all([
+        knex.schema.dropTableIfEsists('users')
+    ])
+};
