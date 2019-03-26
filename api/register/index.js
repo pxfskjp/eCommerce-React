6d860a1216c9f80724bdb2f702f7026756c65b4e
topdeveloper0729 <topdev0729@gmail.com>
2019-03-25 22:31:48 -0700

    register user enpoint created.

--- api/register/index.js
+++ api/register/index.js
@@ -1,6 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const db = require('../../db/helpers/register');
 
-router.post('/', async (req, res) => {
+router.post('/', (req, res) => {
+    let { firstName, lastName, email, uid } = req.body;
+    let image_id = 1;   // use default profile image until image upload is built
     
+    let newUser = {
+        firstName: firstName,
+        lastName: lastName,
+        email: email,  
+        image_id: image_id,
+        uid: uid
+    };
+
+    db.insert(newUser)
+        .then(response => {
+            console.log(response);
+            res.status(200).json(response);
+        })
+        .catch(error => {  // catch error from insert new rep request
+            console.log(error.message);
+            res.status(500).json({message: error.message});
+        })
 })
