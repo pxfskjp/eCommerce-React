eb153715930316475107bd690c9335c1bcecca09
topdeveloper0729 <topdev0729@gmail.com>
2019-03-25 21:36:08 -0700

    one image seed file.

--- db/seeds/dev/01_images.js
+++ db/seeds/dev/01_images.js
@@ -5,7 +5,7 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('images').insert([
-        {id: 1, url: 'https://seeklogo.com/images/N/nodejs-logo-FBE122E377-seeklogo.com.png'}
+        {id: 1, url: 'https://t4.ftcdn.net/jpg/02/15/84/43/240_F_215844325_ttX9YiIIyeaR7Ne6EaLLjMAmy4GvPC69.jpg'}
       ]);
     });
 };
