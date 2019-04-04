41a8359b0e05f9b65536b5bb4706d2cbfbd96725
topdeveloper0729 <topdev0729@gmail.com>
2019-04-03 21:18:56 -0700

    Port changed to 5000 in server index.js.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const server = require('./server.js');
 
-const PORT = process.env.PORT || 3000;   // server will be hosted locally at localhost:3000
+const PORT = process.env.PORT || 5000;   // in development server will be hosted locally at localhost:5000
 
 server.listen(PORT, () => {
     console.log(`Listening on port: ${PORT}`);
