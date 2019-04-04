0f280e4daaf8eb4f85ec9d2726f5e624719e38d5
topdeveloper0729 <topdev0729@gmail.com>
2019-04-03 22:51:28 -0700

    state.logged check removed.

--- use-my-tools/src/components/RegisterPage.js
+++ use-my-tools/src/components/RegisterPage.js
@@ -22,73 +22,66 @@ class RegisterPage extends Component {
         return (
             <div className="register">
                 <MuiThemeProvider>
-                    {this.state.logged ? (
-                        <Typography variant='display1' align='center' gutterBottom>
-                            Successfully Logged In
-                        </Typography>
-                    ):(
-                        <div>
-                            <div className="register-top-bar">
-                                <Link to="/">
-                                    Home
-                                </Link>
-                                
-                            </div>
-                            
-                            <p className="header">Sign Up</p>
-
-                            <form onSubmit={this.onSubmit}>
-                                <TextField
-                                    style = {{width: '65%'}}
-                                    hintText="Enter your Email"
-                                    floatingLabelText="Email"
-                                    name="email"
-                                    type="text"
-                                    required={true}
-                                    value={this.state.email}
-                                    onChange={this.onChange}
-                                />
-                                <br/>
-
-                                <TextField
-                                    style = {{width: '65%'}}
-                                    hintText="Enter your password"
-                                    floatingLabelText="Password"
-                                    required={true}
-                                    name="password"
-                                    type="password"
-                                    value={this.state.password}
-                                    onChange={this.onChange}
-                                />
-                                <br/>
-
-                                <TextField
-                                    style = {{width: '65%'}}
-                                    hintText="Re-enter your password"
-                                    floatingLabelText="Re-enter password"
-                                    name="password1"
-                                    type="password"
-                                    required={true}
-                                    value={this.state.password1}
-                                    onChange={this.onChange}
-                                />
-                                <br/>
-
-                                <RaisedButton
-                                    className="register-button"
-                                    label="SignUp"
-                                    primary={true}
-                                    type="submit"
-                                    disabled={invalidCondition}
-                                />
-
-                                <p>By signing up, you agree to the Terms and Conditions and Privacy Policy.</p>
-
-                                {error && <p>{error.message}</p>}
-
-                            </form>
+                    <div>
+                        <div className="register-top-bar">
+                            <Link to="/">
+                                Home
+                            </Link>
                         </div>
-                    )}
+                        
+                        <p className="header">Sign Up</p>
+
+                        <form onSubmit={this.onSubmit}>
+                            <TextField
+                                style = {{width: '65%'}}
+                                hintText="Enter your Email"
+                                floatingLabelText="Email"
+                                name="email"
+                                type="text"
+                                required={true}
+                                value={this.state.email}
+                                onChange={this.onChange}
+                            />
+                            <br/>
+
+                            <TextField
+                                style = {{width: '65%'}}
+                                hintText="Enter your password"
+                                floatingLabelText="Password"
+                                required={true}
+                                name="password"
+                                type="password"
+                                value={this.state.password}
+                                onChange={this.onChange}
+                            />
+                            <br/>
+
+                            <TextField
+                                style = {{width: '65%'}}
+                                hintText="Re-enter your password"
+                                floatingLabelText="Re-enter password"
+                                name="password1"
+                                type="password"
+                                required={true}
+                                value={this.state.password1}
+                                onChange={this.onChange}
+                            />
+                            <br/>
+
+                            <RaisedButton
+                                className="register-button"
+                                label="SignUp"
+                                primary={true}
+                                type="submit"
+                                disabled={invalidCondition}
+                            />
+
+                            <p>By signing up, you agree to the Terms and Conditions and Privacy Policy.</p>
+
+                            {error && <p>{error.message}</p>}
+
+                        </form>
+                    </div>
                 </MuiThemeProvider>
             </div>
         );
