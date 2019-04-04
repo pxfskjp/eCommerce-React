56b0efb3da21b6e6648dae9eb18ff227cf8acac3
topdeveloper0729 <topdev0729@gmail.com>
2019-04-04 15:13:01 -0700

    firebase admin initialization moved to server.js.

--- server.js
+++ server.js
@@ -3,29 +3,58 @@ const cors = require('cors');
 const helmet = require('helmet');
 const morgan = require('morgan');
 
-// const knex = require('./db/db.js');
-
-      
-const server = express();
-
-if (process.env.ENVIRONMENT == 'development') { 
+// Import .env config vars for dev Environment
+if (process.env.ENVIRONMENT === 'development') { 
     require('dotenv').config(); 
 }
+if(process.env.NODE_ENV !== 'production'){
+    require('dotenv').load();
+}
 
-const admin = require('firebase-admin');
+// Firebsae imports:
+const firebase = require("firebase/app");
+require("firebase/auth");
+require("firebase/database");
+const admin = require('firebase-admin');     
 
-const userRoutes = require('./api/users');    // All CRUD endpoints for users
+admin.initializeApp({
+    credential: admin.credential.cert({
+       projectId: process.env.FIREBASE_PROJECT_ID,
+       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+       privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    }),
+    databaseURL: process.env.FIREBASE_DB_URL
+});
+   
+   
+const firebaseConfig = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DB_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,	
+};
+
+firebase.initializeApp(firebaseConfig);    
+
+// Create Express server
+const server = express();
 
 server.use(express.json());
 server.use(morgan('dev'));
 server.use(cors());
 server.use(helmet());
 
+// Sanity check to see if base URL is live:
 server.get('/',(req, res) => {
     res.send("Server base URL is working...");
 });
 
-// Verify requests using Firebase auth:
+// Import API route/endpoint files:
+const userRoutes = require('./api/users');    // All CRUD endpoints for users
+
+// Verify requests using Firebase-admin auth:
 server.use(async(req,res) => {
     const idToken = req.headers.authorization;  
     try {
