61bb5a4003fa53af84fc539ba6281c26ecfe230f
topdeveloper0729 <topdev0729@gmail.com>
2019-04-03 21:02:27 -0700

    Firebase folder created. context.js, firebase.js, and index.js created inside.

---
+++ use-my-tools/src/components/Firebase/index.js
@@ -0,0 +1,15 @@
+import FirebaseContext, { withFirebase } from './context';
+import Firebase from './firebase';
+
+export default Firebase;
+
+export { FirebaseContext, withFirebase };
+
+
+
+
+
+
+
+
+
