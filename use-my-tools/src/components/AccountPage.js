94879e15c47336652f30b4c8f6fbb32c1d73c868
topdeveloper0729 <topdev0729@gmail.com>
2019-04-03 19:59:16 -0700

    react-router-dom installed.

---
+++ use-my-tools/src/components/AccountPage.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class AccountPage extends Component {
+    render() {
+        return (
+            <div className="account-page-container">
+            </div>
+        );
+    }
+}
+
+export default AccountPage;
