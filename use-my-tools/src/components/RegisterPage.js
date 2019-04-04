715297ebd9c9723e5eee9b247b73c8f00f27559c
topdeveloper0729 <topdev0729@gmail.com>
2019-04-03 22:37:15 -0700

    Register componenent boilerplate form and inputs added.

--- use-my-tools/src/components/RegisterPage.js
+++ use-my-tools/src/components/RegisterPage.js
@@ -3,9 +3,76 @@ import React, { Component } from 'react';
 class RegisterPage extends Component {
     render() {
         return (
-            <div className="register-page-container">
-                <h1>Sign up for an account</h1>
-                
+            <div className="register">
+                <MuiThemeProvider>
+                    {this.state.logged ? (
+                        <Typography variant='display1' align='center' gutterBottom>
+                            Successfully Logged In
+                        </Typography>
+                    ):(
+                        <div>
+                            <div className="register-top-bar">
+                                <Link to="/">
+                                    Home
+                                </Link>
+                                
+                            </div>
+                            
+                            <p className="header">Sign Up</p>
+
+                            <form onSubmit={this.onSubmit}>
+                                <TextField
+                                    style = {{width: '65%'}}
+                                    hintText="Enter your Email"
+                                    floatingLabelText="Email"
+                                    name="email"
+                                    type="text"
+                                    required={true}
+                                    value={this.state.email}
+                                    onChange={this.onChange}
+                                />
+                                <br/>
+
+                                <TextField
+                                    style = {{width: '65%'}}
+                                    hintText="Enter your password"
+                                    floatingLabelText="Password"
+                                    required={true}
+                                    name="password"
+                                    type="password"
+                                    value={this.state.password}
+                                    onChange={this.onChange}
+                                />
+                                <br/>
+
+                                <TextField
+                                    style = {{width: '65%'}}
+                                    hintText="Re-enter your password"
+                                    floatingLabelText="Re-enter password"
+                                    name="password1"
+                                    type="password"
+                                    required={true}
+                                    value={this.state.password1}
+                                    onChange={this.onChange}
+                                />
+                                <br/>
+
+                                <RaisedButton
+                                    className="register-button"
+                                    label="SignUp"
+                                    primary={true}
+                                    type="submit"
+                                    disabled={condition}
+                                />
+
+                                <p>By signing up, you agree to the Terms and Conditions and Privacy Policy.</p>
+
+                                {error && <p>{error.message}</p>}
+
+                            </form>
+                        </div>
+                    )}
+                </MuiThemeProvider>
             </div>
         );
     }
