8f06767dc00e6d7cdcf7519c39d55762652465fb
topdeveloper0729 <topdev0729@gmail.com>
2019-04-03 20:32:41 -0700

    RegisterPage created with minimum boilerplate.

---
+++ use-my-tools/src/components/Register.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class RegisterPage extends Component {
+    render() {
+        return (
+            <div className="register-page-container">
+                <h1>Sign up for an account</h1>
+                
+            </div>
+        );
+    }
+}
+
+export default RegisterPage;
