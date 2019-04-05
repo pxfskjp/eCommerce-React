1b27b1cb441cfbe615b4981b0cbcbc3d44fa0326
topdeveloper0729 <topdev0729@gmail.com>
2019-04-04 20:36:28 -0700

    nav bar added to accountPage.

--- use-my-tools/src/components/AccountPage.js
+++ use-my-tools/src/components/AccountPage.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
+import { Link, withRouter } from "react-router-dom"
+
 
 class AccountPage extends Component {
     render() {
         return (
             <div className="account-page-container">
+                <header className="nav-container">
+                    <Link to="/register" className="nav-link">
+                        Sign Up
+                    </Link>
+                    <br/>
+
+                    <Link to="/login" className="nav-link">
+                        Sign In
+                    </Link>
+                </header>
                 <h1>Welcome to your account</h1>
             </div>
         );
     }
 }
 
-export default AccountPage;
+export default withRouter(AccountPage);
