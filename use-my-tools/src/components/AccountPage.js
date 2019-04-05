b16b3b890288de743820fa33bb442da494030d63
topdeveloper0729 <topdev0729@gmail.com>
2019-04-04 20:38:30 -0700

    LogoutButton added to accountPage.

--- use-my-tools/src/components/AccountPage.js
+++ use-my-tools/src/components/AccountPage.js
@@ -1,20 +1,13 @@
 import React, { Component } from 'react';
 import { Link, withRouter } from "react-router-dom"
-
+import LogoutButton from './LogoutButton';
 
 class AccountPage extends Component {
     render() {
         return (
             <div className="account-page-container">
                 <header className="nav-container">
-                    <Link to="/register" className="nav-link">
-                        Sign Up
-                    </Link>
-                    <br/>
-
-                    <Link to="/login" className="nav-link">
-                        Sign In
-                    </Link>
+                    <LogoutButton />
                 </header>
                 <h1>Welcome to your account</h1>
             </div>
