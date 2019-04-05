5cc83b015c0b929d9255da8a9788364c46d1532a
topdeveloper0729 <topdev0729@gmail.com>
2019-04-04 20:13:26 -0700

    Basic content added to LandingPage.

--- use-my-tools/src/components/LandingPage.js
+++ use-my-tools/src/components/LandingPage.js
@@ -25,6 +25,20 @@ class LandingPage extends Component {
                     <h1 className="welcome-title">
                         Use My Tools - the best way to borrow and rent tools in your area!
                     </h1>
+                    <br/>
+                    <h3>
+                        Browse and search for available tools near you.
+                    </h3>
+                    <br/>
+                    <h3>
+                        Rent or borrow tools for a designated period of time.
+                    </h3>
+                    <br/>
+                    <h3>
+                        Have extra tools lying around? Post them for others to rent and start making money!
+                    </h3>
+                    <br/>
+                    
                 </section>
             </div>
         );
