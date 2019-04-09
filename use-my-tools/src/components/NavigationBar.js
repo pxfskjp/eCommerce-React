64f5baddc2677beeef2d4b2b94652af0feb38c6c
topdeveloper0729 <topdev0729@gmail.com>
2019-04-08 20:38:24 -0700

    NavigationBar file created and AccountPage css file created.

---
+++ use-my-tools/src/components/NavigationBar.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { Link, withRouter } from "react-router-dom"
+import LogoutButton from './LogoutButton';
+import axios from 'axios';
+
+
+class NavigationBar extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            user: null
+        };
+    }
+
+    render() {
+        return (
+            
+            <header className="nav-container">
+                <LogoutButton />
+            </header>
+
+
+        );
+    }
+}
+
+export default withRouter(AccountPage);
