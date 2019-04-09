ab7547213eb1833e00cfa9e3fb722945da687c7f
topdeveloper0729 <topdev0729@gmail.com>
2019-04-09 16:36:18 -0700

    unique constraint added to uid in users table. New migrations run successfully.

--- db/migrations/20190206195013_tools-table.js
+++ db/migrations/20190206195013_tools-table.js
@@ -3,8 +3,7 @@ exports.up = function(knex, Promise) {
     return Promise.all([
         knex.schema.createTable('tools', table => {
             table.increments('id').primary();           // id of the tool
-            table.integer('owner_uid')                   // id of the tool's owner
-                .notNullable()                          // every tool has an owner
+            table.string('owner_uid')                   // id of the tool's owner
                 .references('uid')                      // reference's owner's uid
                 .inTable('users');
             table.integer('renter_id');                  // id of the tool's current renter; a tool might be rented or it might be available
@@ -29,7 +28,7 @@ exports.up = function(knex, Promise) {
         })
     ])
 };
-// about to run create tools table migration
+
 exports.down = function(knex, Promise) {
     return Promise.all([
         knex.schema.dropTableIfEsists('tools')
