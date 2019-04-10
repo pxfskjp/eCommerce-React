8c72688586054cd2766badbe63008bafb1a9bfb2
topdeveloper0729 <topdev0729@gmail.com>
2019-04-09 22:33:03 -0700

    addToolImage created, should return tool_id from tool-images table.

--- db/helpers/images.js
+++ db/helpers/images.js
@@ -1,12 +1,20 @@
 const db = require('../db.js');
 
 module.exports = {
-    addImage
+    addImage,
+    addToolImage
 }
 
 function addImage(image) {
     return db('images')
         .insert(image)
         .returning('id')
+        .then(ids => ids[0]);
+}
+
+function addToolImage(image) {
+    return db('tool_images')
+        .insert(image)
+        .returning('tool_id')
         .then(ids => ids[0]);
 }
