34132381df7f540871cfd5bdbf77edffe2dba1a8
topdeveloper0729 <topdev0729@gmail.com>
2019-04-14 20:36:00 -0700

    getMyTools heler created.

--- db/helpers/tools.js
+++ db/helpers/tools.js
@@ -1,7 +1,8 @@
 const db = require('../db.js');
 
 module.exports = {
-    createTool
+    createTool,
+    getMyTools
 }
 
 function createTool(newTool) {
@@ -9,4 +10,9 @@ function createTool(newTool) {
         .insert(newTool)
         .returning('id')
         .then(ids => ids[0]);
+}
+
+function getMyTools(uid) {
+    return db('tools')
+        .where('owner_uid', uid);
 }
