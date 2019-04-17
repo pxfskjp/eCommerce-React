0638025b6e30562ac328f7b75ce50f1b33f7a7cf
topdeveloper0729 <topdev0729@gmail.com>
2019-04-16 23:07:52 -0700

    AccountPage account info fields added and with basic styling in left container.

--- use-my-tools/src/components/AccountPage.js
+++ use-my-tools/src/components/AccountPage.js
@@ -1,9 +1,28 @@
 import React, { Component } from 'react';
 import { Link, withRouter } from "react-router-dom"
+import { withStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
 
 import axios from 'axios';
 
 import "./css/AccountPage.css";
+
+const styles = theme => ({
+    container: {
+      display: "flex"
+    },
+    textField: {
+      marginLeft: theme.spacing.unit,
+      marginRight: theme.spacing.unit
+    },
+    dense: {
+      marginTop: 16
+    },
+    menu: {
+      width: 200
+    }
+});
 
 class AccountPage extends Component {
     constructor(props) {
@@ -13,7 +32,8 @@ class AccountPage extends Component {
             firstName: '',
             lastName: '',
             email: '',
-            image_url: '',
+            homeStreetAddress: '',
+            imageUrl: '',
             selectedFile: null,
             error: null,
         };
@@ -23,21 +43,84 @@ class AccountPage extends Component {
         axios.get('/api/users/userinfo')
             .then(user => {
                 this.setState({
-                    user: user.data
-                }, () => console.log('AccountPage state.user after GET user info: ', this.state.user)) ;
+                    user: user.data,
+                    firstName: user.data.firstname,
+                    lastName: user.data.lastname,
+                    email: user.data.email,
+                    homeStreetAddress: user.data.home_street_address,
+                    imageUrl: user.data.image_url,
+                }, () => console.log('AccountPage state after GET user info: ', this.state)) ;
             })
             .catch(error => {
                 console.log(error.message);
             })
     }
 
+    handleChange = name => event => {
+        this.setState({
+          [name]: event.target.value
+        });
+    };
+
     render() {
+        const { classes } = this.props;
         return (
             <div className="account-page-container">
-                <h1>Welcome to your account</h1>
+
+                <div className="left-container">
+                    <form onSubmit={this.updateUserDetails}>
+                        <h2>Edit Account Information</h2>
+                        <TextField
+                            id="outlined-first-name"
+                            label="First Name"
+                            className={classes.textField}
+                            value={this.state.firstName}
+                            onChange={this.handleChange("firstName")}
+                            margin="normal"
+                            variant="outlined"
+                        />
+
+                        <TextField
+                            id="outlined-last-name"
+                            label="Last Name"
+                            className={classes.textField}
+                            value={this.state.lastName}
+                            onChange={this.handleChange("lastName")}
+                            margin="normal"
+                            variant="outlined"
+                        />
+
+                        <TextField
+                            id="outlined-email"
+                            label="Email"
+                            className={classes.textField}
+                            value={this.state.email}
+                            onChange={this.handleChange("email")}
+                            margin="normal"
+                            variant="outlined"
+                            InputProps={{
+                                readOnly: true,
+                            }}
+                        />
+
+                        <TextField
+                            id="outlined-home-street-address"
+                            label="Street Address"
+                            className={classes.textField}
+                            value={this.state.home_street_address}
+                            onChange={this.handleChange("home_street-address")}
+                            margin="normal"
+                            variant="outlined"
+                        />
+
+                        <Button variant="outlined" color="primary" className="save-button" type="submit" >
+                            Save
+                        </Button>
+                    </form>
+                </div> 
             </div>
         );
     }
 }
 
-export default withRouter(AccountPage);
+export default withStyles(styles)(withRouter(AccountPage));
