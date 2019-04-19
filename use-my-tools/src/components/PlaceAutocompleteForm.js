4bbbd1ab8f67292e740aca9a6545339ecbe93398
topdeveloper0729 <topdev0729@gmail.com>
2019-04-18 21:21:56 -0700

    PlaceAutocompleteForm component created for development.

---
+++ use-my-tools/src/components/PlaceAutocompleteForm.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+class PlaceAutocompleteForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { address: '' }
+      }
+}
