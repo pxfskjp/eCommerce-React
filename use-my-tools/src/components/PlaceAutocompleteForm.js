17b08ded2bf07c7da1db5d3c91a81b01ddd13382
topdeveloper0729 <topdev0729@gmail.com>
2019-04-18 21:35:37 -0700

    react-places-autocomplete library added to dependencies.

--- use-my-tools/src/components/PlaceAutocompleteForm.js
+++ use-my-tools/src/components/PlaceAutocompleteForm.js
@@ -3,6 +3,33 @@ import React from 'react';
 class PlaceAutocompleteForm extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { address: '' }
-      }
+        this.state = { address: '' };
+
+        // Specify form input field:
+        const autocompleteFormField = document.getElementById(`street-address-field`);  
+
+        // Instantiate a new autocomplete object:
+        const autocomplete = new google.maps.places.Autocomplete((autocompleteFormField), {
+            types: [`address`],
+            componentRestrictions: [`us`],
+        });     
+
+        // Clearing the listener ensures that the results that are returned always correspond to the latest user input.
+        // Depending on your implementation, you might need to call this method several times
+        google.maps.event.clearInstanceListeners(autocompleteFormField);
+        
+        // Add the custom Google Places listener, place_changed, 
+        // which will activate when a result from the autocomplete list is selected:
+        google.maps.event.addListener(autocomplete, `place_changed`, () => {
+            // Custom methods to populate form fields.
+        })
+        
+    }
+
+    // componentDidMount() {
+        
+    // }
+
 }
+
+export default PlaceAutocompleteForm;
