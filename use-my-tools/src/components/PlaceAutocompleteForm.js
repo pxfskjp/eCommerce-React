ac7a7c88a1ae1b3dcd67b4c6b7b06e279f79dc14
topdeveloper0729 <topdev0729@gmail.com>
2019-04-18 21:41:10 -0700

    react-places-autocomplete starter code added.

--- use-my-tools/src/components/PlaceAutocompleteForm.js
+++ use-my-tools/src/components/PlaceAutocompleteForm.js
@@ -1,35 +1,100 @@
 import React from 'react';
+import PlacesAutocomplete, {
+  geocodeByAddress,
+  getLatLng,
+} from 'react-places-autocomplete';
 
-class PlaceAutocompleteForm extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { address: '' };
+class LocationSearchInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { address: '' };
+  }
 
-        // Specify form input field:
-        const autocompleteFormField = document.getElementById(`street-address-field`);  
+  handleChange = address => {
+    this.setState({ address });
+  };
 
-        // Instantiate a new autocomplete object:
-        const autocomplete = new google.maps.places.Autocomplete((autocompleteFormField), {
-            types: [`address`],
-            componentRestrictions: [`us`],
-        });     
+  handleSelect = address => {
+    geocodeByAddress(address)
+      .then(results => getLatLng(results[0]))
+      .then(latLng => console.log('Success', latLng))
+      .catch(error => console.error('Error', error));
+  };
 
-        // Clearing the listener ensures that the results that are returned always correspond to the latest user input.
-        // Depending on your implementation, you might need to call this method several times
-        google.maps.event.clearInstanceListeners(autocompleteFormField);
-        
-        // Add the custom Google Places listener, place_changed, 
-        // which will activate when a result from the autocomplete list is selected:
-        google.maps.event.addListener(autocomplete, `place_changed`, () => {
-            // Custom methods to populate form fields.
-        })
-        
-    }
-
-    // componentDidMount() {
-        
-    // }
-
+  render() {
+    return (
+      <PlacesAutocomplete
+        value={this.state.address}
+        onChange={this.handleChange}
+        onSelect={this.handleSelect}
+      >
+        {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
+          <div>
+            <input
+              {...getInputProps({
+                placeholder: 'Search Places ...',
+                className: 'location-search-input',
+              })}
+            />
+            <div className="autocomplete-dropdown-container">
+              {loading && <div>Loading...</div>}
+              {suggestions.map(suggestion => {
+                const className = suggestion.active
+                  ? 'suggestion-item--active'
+                  : 'suggestion-item';
+                // inline style for demonstration purpose
+                const style = suggestion.active
+                  ? { backgroundColor: '#fafafa', cursor: 'pointer' }
+                  : { backgroundColor: '#ffffff', cursor: 'pointer' };
+                return (
+                  <div
+                    {...getSuggestionItemProps(suggestion, {
+                      className,
+                      style,
+                    })}
+                  >
+                    <span>{suggestion.description}</span>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        )}
+      </PlacesAutocomplete>
+    );
+  }
 }
 
-export default PlaceAutocompleteForm;
+// class PlaceAutocompleteForm extends React.Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = { address: '' };
+
+//         // Specify form input field:
+//         const autocompleteFormField = document.getElementById(`street-address-field`);  
+
+//         // Instantiate a new autocomplete object:
+//         const autocomplete = new google.maps.places.Autocomplete((autocompleteFormField), {
+//             types: [`address`],
+//             componentRestrictions: [`us`],
+//         });     
+
+//         // Clearing the listener ensures that the results that are returned always correspond to the latest user input.
+//         // Depending on your implementation, you might need to call this method several times
+//         google.maps.event.clearInstanceListeners(autocompleteFormField);
+
+//         // Add the custom Google Places listener, place_changed, 
+//         // which will activate when a result from the autocomplete list is selected:
+//         google.maps.event.addListener(autocomplete, `place_changed`, () => {
+//             // Custom methods to populate form fields.
+//         })
+        
+//     }
+
+//     // componentDidMount() {
+        
+//     // }
+
+// }
+
+// export default PlaceAutocompleteForm;
