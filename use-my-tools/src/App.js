5b1e79043e0b5405b0a76a1a9fbb4853a98ce20b
topdeveloper0729 <topdev0729@gmail.com>
2019-04-29 22:16:36 -0500

    toolviewowner route added to App.js.

--- use-my-tools/src/App.js
+++ use-my-tools/src/App.js
@@ -19,6 +19,8 @@ import MyTools from './components/tools/MyTools';
 import AddTool from './components/tools/AddTool';
 import FindTools from './components/tools/FindTools';
 import ToolViewRenter from './components/tools/ToolViewRenter';
+import ToolViewOwner from './components/tools/ToolViewOwner';
+
 
 import DateRangePickerWrapper from './components/ReactDates/DateRangePicker';
 
@@ -88,6 +90,7 @@ class AppComponentBase extends Component {
               <Route path={"/updatepassword"} component={UpdatePassword} />
               <Route path={"/findtools"} component={FindTools} />
               <Route path={"/toolviewrenter/:id"} component={ToolViewRenter} />
+              <Route path={"/toolviewowner/:id"} component={ToolViewOwner} />
 
               <Route path={"/dates"} component={DateRangePickerWrapper} />
             </Router>
