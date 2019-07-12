3d3de8701579716aac9bfce7655b97dfb8622001
topdeveloper0729 <topdev0729@gmail.com>
2019-07-12 00:49:26 -0700

    profile pic width reduced on account page.

--- use-my-tools/src/components/tools/ToolViewOwner.js
+++ use-my-tools/src/components/tools/ToolViewOwner.js
@@ -5,7 +5,6 @@ import Card from "@material-ui/core/Card";
 import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
 import Paper from '@material-ui/core/Paper';
-// import Grid from "@material-ui/core/Grid";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 import ImageCarousel from './ImageCarousel';
