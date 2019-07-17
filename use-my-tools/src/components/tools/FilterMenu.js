115c307ac204ae2eff77a7416da3f02e8a2e2b55
topdeveloper0729 <topdev0729@gmail.com>
2019-07-17 00:18:28 -0700

    FilterMenu component created with rudiemntary elements.

---
+++ use-my-tools/src/components/tools/FilterMenu.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+
+class FilterMenu extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            currentMaxPriceInput,
+            currentKeywordInput,
+            maxPrice: 100,
+            keywords: []
+        };
+    }
+
+    render() {
+        return (
+            <div className="filter-menu-container">
+                <h3>Filter Results</h3>
+
+                <form className="max-price-form">
+                    <div className="max-price-indicator">
+                        <p>Max Price: ${this.state.maxPrice}</p>
+                    </div>
+                    <input type="number" value={this.state.currentMaxPriceInput}/>
+                    <button type="submit"/>
+                </form>
+
+                <p>Keyword:</p>
+                <form className="keyword-form">
+                    <input type="text" value={this.state.currentKeywordInput}/>
+                    <button type="submit"/>
+                </form>
+
+            </div>
+        )
+    }
+}
+
+export default FilterMenu;
