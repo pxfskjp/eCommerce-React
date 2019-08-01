5a179ad4414dec779eafc29708ea23bb8653b0e6
topdeveloper0729 <topdev0729@gmail.com>
2019-07-31 22:36:02 -0700

    RenterDashboard created as cuntional component.

--- use-my-tools/src/components/Owner/OwnerDashboard.js
+++ use-my-tools/src/components/Owner/OwnerDashboard.js
@@ -2,10 +2,10 @@ import React, { Component } from 'react';
 import { withRouter, Link } from "react-router-dom"
 import { withStyles } from "@material-ui/core/styles";
 
-import Paper from "@material-ui/core/Paper";
+// import Paper from "@material-ui/core/Paper";
 import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
-import Typography from "@material-ui/core/Typography";
+// import Typography from "@material-ui/core/Typography";
 
 import MyTools from './MyTools';
 import RentalsView from '../Rentals/RentalsView';
