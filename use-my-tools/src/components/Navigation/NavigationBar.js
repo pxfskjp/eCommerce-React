2964b6ec7d7c1ffd9006e7001309d57ab880c262
topdeveloper0729 <topdev0729@gmail.com>
2019-09-09 20:45:41 -0700

    Created NavAuth component and css file.

--- use-my-tools/src/components/Navigation/NavigationBar.js
+++ use-my-tools/src/components/Navigation/NavigationBar.js
@@ -2,11 +2,12 @@ import React, { Component } from 'react';
 import { NavLink, withRouter } from "react-router-dom";
 import LogoutButton from '../LogoutButton';
 import NavNonAuth from './NavNonAuth';
+import NavAuth from './NavAuth';
 import MobileNavMenu from './MobileNavMenu';
-import "./css/NavigationBar.css";
+
 
 const NavigationBar = props => (
-    <div>{props.authUser ? <NavigationAuth /> : <NavNonAuth />}</div>
+    <div>{props.authUser ? <NavAuth /> : <NavNonAuth />}</div>
 );
 
 const NavigationAuth = () => (
@@ -41,77 +42,4 @@ const NavigationAuth = () => (
     </header>
 );
 
-const NavigationNonAuth = () => (
-    <div className="nav-container">
-        <h1 className="logo">Use My Tools</h1>
-        <div className="nav-link-container-non-auth">
-            <NavLink to="/" className="nav-link">
-                Home
-            </NavLink>
-            <NavLink to="/register" className="nav-link">
-                Sign Up
-            </NavLink>
-            <NavLink to="/login" className="nav-link">
-                Sign In
-            </NavLink>
-        </div>
-    </div>
-);
-
 export default withRouter(NavigationBar);
-
-// class NavigationBar extends Component {
-//     constructor(props) {
-//         super(props);
-//         this.state = {
-//             user: null,
-//             showNavLinks: true
-//         };
-//     }
-
-//     toggleShowNavLinks = event => {
-//         const style = (this.state.showNavLinks === true) ? false : true;
-//         this.setState({ showNavLinks: style });
-//     }
-
-//     render() {
-//         return (
-            
-//             <header className="nav-container">
-//                 <h1 className="logo">Use My Tools</h1>
-//                 {/* <button className="nav-menu-btn" onClick={this.toggleShowNavLinks}>
-//                     Menu
-//                 </button> */}
-//                 <div className="nav-link-container">
-                    
-//                     <NavLink to="/findtools" className="nav-link" activeStyle={{ color: 'white' }}>
-//                         Find Tools
-//                     </NavLink>
-
-//                     <NavLink to="/addtool" className="nav-link" activeStyle={{ color: 'white' }}>
-//                         Add a tool
-//                     </NavLink>
-
-//                     <NavLink to="/ownerdashboard" className="nav-link" activeStyle={{ color: 'white' }}>
-//                         Owner Dashboard
-//                     </NavLink>
-
-//                     <NavLink to="/renterdashboard" className="nav-link" activeStyle={{ color: 'white' }}>
-//                         Renter Dashboard
-//                     </NavLink>
-
-//                     <NavLink to="/chat" className="nav-link" activeStyle={{ color: 'white' }}>
-//                         Messages
-//                     </NavLink>
-                    
-//                     <NavLink to="/accountpage" className="nav-link" activeStyle={{ color: 'white' }}>
-//                         Account
-//                     </NavLink>
-//                     <LogoutButton className="nav-link" />
-//                 </div>
-//             </header>
-
-
-//         );
-//     }
-// }
